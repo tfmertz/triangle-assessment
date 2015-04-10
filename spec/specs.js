c4d3a7b191bfe61464808c7492e0c556bcdd608e
Tom Mertz <tom.mertz@verizon.net>
2015-04-10 09:29:11 -0700

    Add basic html and isoscles test

--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,13 @@ describe('findTriangle', function() {
   it('takes a 1, 1, 1, and returns "Equilateral"', function() {
     expect(findTriangle(1,1,1)).to.equal("Equilateral");
   });
+
+  it('takes a 5, 5, 5, and returns "Equilateral"', function() {
+    expect(findTriangle(5,5,5)).to.equal("Equilateral");
+  });
+
+  it('takes a 2, 1, 1, and returns "Isosceles"', function() {
+    expect(findTriangle(2,1,1)).to.equal("Isosceles");
+  });
+
 });
