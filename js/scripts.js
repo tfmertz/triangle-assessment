e7d7da8d0f44e730bdb99218b2e58b03aec5823e
Tom Mertz <tom.mertz@verizon.net>
2015-04-10 09:41:23 -0700

    Add invalid check and specs

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,16 @@
 
 function findTriangle(sideA, sideB, sideC) {
 
+  //check if the triangle is valid
+  var side_array = [sideA, sideB, sideC];
+  side_array.sort();
+    return "Invalid";
+  if(side_array[0] + side_array[1] <= side_array[2]) {
+  }
+
   //if the left side evaluates to true the string will always be true
   //and will get returned
   return ((sideA === sideB && sideB === sideC) && 'Equilateral') ||
-         ((sideA === sideB || sideB === sideC || sideA === sideC) && 'Isosceles');
+         ((sideA === sideB || sideB === sideC || sideA === sideC) && 'Isosceles') ||
+         'Scalene';
 }
--- spec/specs.js
+++ spec/specs.js
@@ -19,4 +19,16 @@ describe('findTriangle', function() {
     expect(findTriangle(1,2,1)).to.equal("Isosceles");
   });
 
+  it('takes a 3, 2, 1, and returns "Scalene"', function() {
+    expect(findTriangle(3,2,1)).to.equal("Scalene");
+  });
+
+  it('takes a 5, 4, 3, and returns "Scalene"', function() {
+    expect(findTriangle(5,4,3)).to.equal("Scalene");
+  });
+
+  it('takes a 2, 2, 8, and returns "Invalid"', function() {
+    expect(findTriangle(2,2,8)).to.equal("Invalid");
+  });
+
 });
